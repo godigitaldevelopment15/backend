b176a63546fff8188cd97f26e504d225c9e6ec58
godigitaldevelopment15 <godigitaldevelopment15@gmail.com>
2025-01-16 17:26:48 +0530

    Add files via upload
    d

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors"); // Import the cors middleware
 
 const corsOptions = {
-  origin: ['http://localhost:3000'], 
+  origin: ['*'], 
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type, Authorization',
